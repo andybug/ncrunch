95334e3f59eea18f56ec7d8b350720a06526ca79
Andrew Fields <andybug10@gmail.com>
2012-07-28 11:02:47 -0500

    Remove SHA code from main.c

--- src/main.c
+++ src/main.c
@@ -4,8 +4,6 @@
 #include <assert.h>
 #include <unistd.h>
 
-#include <openssl/sha.h>
-
 #include <ncaacrunch.h>
 
 
@@ -108,28 +106,6 @@ static int _check_installed_version(void)
 	return -4;
 
 #endif	/* end OS check */
-}
-
-
-static void show_digest(unsigned char* digest)
-{
-	int high, low;
-	int i;
-	static char trans[] = { '0', '1', '2', '3',
-				'4', '5', '6', '7',
-				'8', '9', 'a', 'b',
-				'c', 'd', 'e', 'f' };
-
-
-	for(i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-		high = (digest[i] & 0xf0) >> 4;
-		low = digest[i] & 0x0f;
-
-		putchar(trans[high]);
-		putchar(trans[low]);
-	}
-
-	putchar('\n');
 }
 
 
