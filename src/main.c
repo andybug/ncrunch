752159ee117488e79d9f13642a9ecbce351c4da6
Andrew Fields <andybug10@gmail.com>
2012-08-26 21:53:13 -0500

    Removed version-checking code from main.c: it was unnecessary

--- src/main.c
+++ src/main.c
@@ -9,119 +9,6 @@
 
 
 
-/**
- * Determines the full path to the executable on the system
- *
- * @param location The buffer to put the path into
- */
-
-static int _get_exe_location(char location[FILENAME_MAX])
-{
-#ifdef __linux__
-	int error;
-
-	error = readlink("/proc/self/exe", location, FILENAME_MAX);
-	if (error < 0) {
-		fprintf(stderr, "%s: Unable to read /proc/self/exe\n", __func__);
-		return -1;
-	}
-
-	return 0;
-
-#else	/* OS not supported */
-	return -1;
-
-#endif
-}
-
-
-
-/**
- * Compares the executable's location to the build type to ensure that config
- * files will be read from the correct directories
- */
-
-static int _check_installed_version(void)
-{
-	char exe_location[FILENAME_MAX];
-
-#ifdef __linux__
-	int error;
-	int release = -1;
-
-	error = _get_exe_location(exe_location);
-	if (error < 0) {
-		return -1;
-	}
-
-	if (strncmp("/usr/bin/", exe_location, 9) == 0) {	/* RELEASE */
-		release = 1;
-	}
-
-	else if (strncmp("/opt/", exe_location, 5) == 0) {	/* RELEASE */
-		release = 1;
-	}
-
-	else {							/* DEBUG */
-		release = 0;
-	}
-
-
-	/* ensure that the installed location is equivalent to the build type;
-	 * this will make sure that directory paths throughout the program will
-	 * read from the right locations (configs, etc.)
-	 */
-
-
-#ifdef NCRUNCH_DEBUG
-	if (release) {		/* release location but dev build */
-		fprintf(stderr, "%s: developer build running from release location\n", __func__);
-		return -2;
-	}
-
-#elif defined NCRUNCH_RELEASE
-	if (!release) {		/* dev location but release build */
-		fprintf(stderr, "%s: release build running from developer location\n", __func__);
-		return -2;
-	}
-
-#else
-	/* Undefined build type */
-
-	fprintf(stderr, "%s: Undefined build type\n", __func__);
-	return -3;
-
-#endif	/* end build type test */
-
-
-	/* everything's ok */
-	return 0;
-
-
-	/********** END LINUX *****************************************/
-
-
-
-#else	
-	/* OS not supported */
-	return -4;
-
-#endif	/* end OS check */
-}
-
-
-
-
-
-/**
- * The name of the flat file from the command line
- *
- * It can be set with either the -f flag or simply ncrunch [flatf]
- */
-
-static const char *flatf_name = NULL;
-
-
 
 /**
  * @struct switch_handler
@@ -181,6 +68,15 @@ static struct switch_handler handlers[] = {
 static struct switch_handler *active_switch = NULL;
 
 
+/**
+ * The name of the flat file from the command line
+ *
+ * It can be set with either the -f flag or simply ncrunch [flatf]
+ */
+
+static const char *flatf_name = NULL;
+
+
 
 
 
@@ -192,16 +88,15 @@ static struct switch_handler *active_switch = NULL;
 
 static void _switch_version(const char *arg)
 {
-	int major = NCRUNCH_VERSION_MAJOR;
-	int minor = NCRUNCH_VERSION_MINOR;
-
-
 	if (arg) {
 		fprintf(stderr, "%s: -v does not take an argument\n", __func__);
 		exit(EXIT_FAILURE);
 	}
 
 #ifdef NCRUNCH_RELEASE
+	int major = NCRUNCH_VERSION_MAJOR;
+	int minor = NCRUNCH_VERSION_MINOR;
+
 	printf("ncrunch v%d.%d", minor, major);
 	printf(" by Andrew Fields - 2012\n");
 #else
@@ -408,15 +303,10 @@ int main(int argc, char** argv)
 
 	_process_args(argc, argv);
 
-	error = _check_installed_version();
-	if (error < 0) {
-		return -1;
-	}
-
 	/* install our exit callback function */
 	atexit(_exit_handler);
-	flatf_read(flatf_name);
+	error = flatf_read(flatf_name);
 
-	return 0;
+	return error;
 }
 
