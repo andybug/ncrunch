4b34b96ca0559ebcff83820e68a27e65f53749b4
Andrew Fields <andybug10@gmail.com>
2012-09-03 17:17:37 -0500

    Updated commenting in ncrunch.h

--- src/include/ncrunch/ncrunch.h
+++ src/include/ncrunch/ncrunch.h
@@ -1,4 +1,12 @@
 
+/**
+ * @file ncrunch.h
+ * @author Andrew Fields
+ *
+ * This file contains all of the main function and structure declarations
+ * for the program.
+ */
+
 #pragma once
 
 #include <ncrunch/hash.h>
@@ -6,8 +14,18 @@
 #define NCRUNCH_VERSION_MAJOR 1
 #define NCRUNCH_VERSION_MINOR 0
 
+/******************************************************************************
+ * TFL FUNCTIONS AND STRUCTURES
+ * Functions defined in teams.c
+ ******************************************************************************
+ */
+
 /**
+ * @enum tfl_type
+ * @brief The field types for the team field list (tfl)
  *
+ * The types are used for the tfl, which also defines the type for each
+ * of the team's entries.
  */
 
 enum tfl_type {
@@ -17,7 +35,16 @@ enum tfl_type {
 };
 
 /**
+ * @struct tfl_entry
+ * @brief The structure used to keep track of the mapping from field name
+ * to type, and vice-versa.
  *
+ * Fields:\n
+ * - name: The name of the field. This will be set from the top line of the flatf,
+ *   which is the heading line. The name is copied, so it needs to be free'd.
+ * - type: The associated type for this field. It should only ever be TEAM_FIELD_STRING
+ *   or TEAM_FIELD_DOUBLE. TEAM_FIELD_INVALID is only used for error-checking purposes.
+ *   The type is determined by the first team entry's field types.
  */
 
 struct tfl_entry {
@@ -32,14 +59,35 @@ int tfl_set_type(size_t id, enum tfl_type type);
 const char *tfl_get_name(size_t id);
 enum tfl_type tfl_get_type(size_t id);
 int tfl_find(const char *name, size_t * id);
-
 void tfl_destroy(void);
+
+/******************************************************************************
+ * TEAM FUNCTIONS AND STRUCTURES
+ * Functions defined in teams.c
+ ******************************************************************************
+ */
 
 #define TEAMS_MAXTEAMS	64
 #define TEAMS_INVALID	TEAMS_MAXTEAMS
 
 /**
+ * @union team_field
+ * @brief Container for the field data.
  *
+ * The team_field union contains the actual data for one of a team's fields.
+ * The field's type can be determined by checking the type of the equivalent
+ * id in the tfl. For instance:\n
+ * @code
+ * size_t teamid = 0;
+ * size_t fieldid = 1; //The field we want
+ * enum tfl_type = tfl_get_type(fieldid);
+ * if (type == TEAM_FIELD_STRING)
+ * 	team_set_string(teamid, fieldid, "easy");
+ * @endcode
+ *
+ * Fields:\n
+ * - data_s: String data
+ * - data_d: Numeric data
  */
 
 union team_field {
@@ -48,7 +96,15 @@ union team_field {
 };
 
 /**
+ * @struct team
+ * @brief A container for a team's fields
  *
+ * Each team_field in fields corresponds to the same id in the tfl. The tfl
+ * stores all of the type information.
+ *
+ * Fields:\n
+ * - name: The hashed value of the name
+ * - fields: The field list for the team
  */
 
 struct team {
@@ -60,10 +116,12 @@ size_t team_create(void);
 void team_destroy(size_t id);
 int team_set_string(size_t id, size_t field, const char *str);
 int team_set_double(size_t id, size_t field, double val);
-
 void teams_destroy(void);
 size_t teams_num_teams(void);
 
-/* Functions for reading flat file that contains team data */
+/******************************************************************************
+ * FLAT FILE FUNCTIONS
+ ******************************************************************************
+ */
 
 int flatf_read(const char *filename);
